07cd2715b6628b05be50b8ab19c41c9e44bc9600
ChristianOkokhere <69167323+ChristianOkokhere@users.noreply.github.com>
2021-12-29 23:43:19 -0600

    small adds

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ app = Flask(__name__, static_url_path='', static_folder='')
 
 @app.route('/')
 def root():
-    return app.send_static_file('index.html')
+    return app.send_static_file('home.html')
+@app.route('/Demographics')
+def page2():
+    return app.send_static_file('yamama.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
